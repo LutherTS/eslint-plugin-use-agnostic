70b37f6c5430d52cfa096200e20d15bdb60bfc19
Luther Tchofo Safo <nintendobserver@gmail.com>
2025-05-17 12:44:01 +0200

    validFilesWithCodes[29] solved. All tests passed manually.

--- tests/agnostic20/import-rules.test.js
+++ tests/agnostic20/import-rules.test.js
@@ -1,6 +1,15 @@
 import fs from "fs";
 import path from "path";
 
+import { RuleTester } from "eslint";
+
+import {
+  enforceEffectiveDirectivesRuleName,
+  importBreaksEffectiveImportRulesMessageId,
+} from "../../library/_commons/constants/bases.js";
+
+import enforceEffectiveDirectivesImportRules from "../../library/agnostic20/_commons/rules/import-rules.js";
+
 const validPath = "./tests/agnostic20/importing/valid";
 const invalidPath = "./tests/agnostic20/importing/invalid";
 
@@ -18,13 +27,37 @@ const readFilesRecursively = (folderPath) =>
     }
   }, []);
 
-const readFilesRecursivelyWithFileCodes = (folderPath) =>
+const readValidFilesRecursively = (folderPath) =>
   readFilesRecursively(folderPath).map((e) => ({
-    filePath: e,
-    fileCode: fs.readFileSync(e, "utf8"),
+    name: e,
+    filename: e,
+    code: fs.readFileSync(e, "utf8"),
   }));
 
-const validFilesWithCodes = readFilesRecursivelyWithFileCodes(validPath);
-const invalidFilesWithCodes = readFilesRecursivelyWithFileCodes(invalidPath);
+const readInvalidFilesRecursively = (folderPath) =>
+  readFilesRecursively(folderPath).map((e) => ({
+    name: e,
+    filename: e,
+    code: fs.readFileSync(e, "utf8"),
+    // errors: 1,
+    errors: [{ messageId: importBreaksEffectiveImportRulesMessageId }],
+  }));
 
-console.log({ validFilesWithCodes, invalidFilesWithCodes });
+const validFiles = readValidFilesRecursively(validPath);
+const invalidFiles = readInvalidFilesRecursively(invalidPath);
+
+const ruleTester = new RuleTester();
+
+ruleTester.run(
+  enforceEffectiveDirectivesRuleName, // rule name
+  enforceEffectiveDirectivesImportRules, // rule code
+  {
+    // checks
+    // 'valid' checks cases that should pass
+    valid: validFiles,
+    // 'invalid' checks cases that should not pass
+    invalid: invalidFiles,
+  }
+);
+
+console.log("All tests passed.");
