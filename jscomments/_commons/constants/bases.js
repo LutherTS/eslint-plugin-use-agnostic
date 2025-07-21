fc9ec986a93b90ed7495536d22934e1282fbe0d4
Luther Tchofo Safo <nintendobserver@gmail.com>
2025-07-22 00:22:35 +0200

    Resolved before moving.

--- jscomments/_commons/constants/bases.js
+++ jscomments/_commons/constants/bases.js
@@ -2,128 +2,128 @@
 export const jSDocComments = Object.freeze({
   // library/_commons/utilities/helpers.js
   basePath:
-    "The absolute import path with its extension yet resolved." /* $COMMENT#JSDOC#BASEPATH */,
+    "The absolute import path with its extension yet resolved." /* The absolute import path with its extension yet resolved. */,
   currentDir:
-    "The directory of the file containing the import, such as from `path.dirname(context.filename)`." /* $COMMENT#JSDOC#CURRENTDIR */,
+    "The directory of the file containing the import, such as from `path.dirname(context.filename)`." /* The directory of the file containing the import, such as from `path.dirname(context.filename)`. */,
   importPath:
-    "The import specifier (e.g., `@/components/Button` or `./utils`), such as one from the current node." /* $COMMENT#JSDOC#IMPORTPATH */,
-  cwd: "The project root, such as from `context.cwd`." /* $COMMENT#JSDOC#CWD */,
+    "The import specifier (e.g., `@/components/Button` or `./utils`), such as one from the current node." /* The import specifier (e.g., `@/components/Button` or `./utils`), such as one from the current node. */,
+  cwd: "The project root, such as from `context.cwd`." /* The project root, such as from `context.cwd`. */,
   fileResolvedPath:
-    "The resolved path of the file." /* $COMMENT#JSDOC#FILERESOLVEDPATH */,
-  aContext: "An ESLint rule's `context` object." /* $COMMENT#JSDOC#ACONTEXT */,
+    "The resolved path of the file." /* The resolved path of the file. */,
+  aContext: "An ESLint rule's `context` object." /* An ESLint rule's `context` object. */,
   resolvedDirectives_blockedImports:
-    "The blocked imports object, either for agnostic20 or for directive21." /* $COMMENT#JSDOC#RESOLVEDDIRECTIVES_BLOCKEDIMPORTS */,
+    "The blocked imports object, either for agnostic20 or for directive21." /* The blocked imports object, either for agnostic20 or for directive21. */,
   currentFileResolvedDirective:
-    'The current file\'s "resolved" directive.' /* $COMMENT#JSDOC#CURRENTFILERESOLVEDDIRECTIVE */,
+    'The current file\'s "resolved" directive.' /* The current file's "resolved" directive. */,
   importedFileResolvedDirective:
-    'The imported file\'s "resolved" directive.' /* $COMMENT#JSDOC#IMPORTEDFILERESOLVEDDIRECTIVE */,
+    'The imported file\'s "resolved" directive.' /* The imported file's "resolved" directive. */,
   theCurrentFileResolvedDirective:
-    'The "resolved" directive of the "resolved" module.' /* $COMMENT#JSDOC#THECURRENTFILERESOLVEDDIRECTIVE */,
+    'The "resolved" directive of the "resolved" module.' /* The "resolved" directive of the "resolved" module. */,
   // library/ ... /config.js
-  plugin: "The use-agnostic ESLint plugin itself." /* $COMMENT#JSDOC#PLUGIN */,
+  plugin: "The use-agnostic ESLint plugin itself." /* The use-agnostic ESLint plugin itself. */,
   // library/agnostic20/_commons/constants/bases.js
   currentFileEffectiveDirective:
-    "The current file's effective directive." /* $COMMENT#JSDOC#CURRENTFILEEFFECTIVEDIRECTIVE */,
+    "The current file's effective directive." /* The current file's effective directive. */,
   importedFileEffectiveDirective:
-    "The imported file's effective directive." /* $COMMENT#JSDOC#IMPORTEDFILEEFFECTIVEDIRECTIVE */,
+    "The imported file's effective directive." /* The imported file's effective directive. */,
   // library/ ... /flows.js
-  context: "The ESLint rule's `context` object." /* $COMMENT#JSDOC#CONTEXT */,
-  node: "The ESLint `node` of the rule's current traversal." /* $COMMENT#JSDOC#NODE */,
+  context: "The ESLint rule's `context` object." /* The ESLint rule's `context` object. */,
+  node: "The ESLint `node` of the rule's current traversal." /* The ESLint `node` of the rule's current traversal. */,
   // library/ ... /helpers.js
   extension:
-    "The JavaScript (TypeScript) extension of the file." /* $COMMENT#JSDOC#EXTENSION */,
+    "The JavaScript (TypeScript) extension of the file." /* The JavaScript (TypeScript) extension of the file. */,
   resolvedPath:
-    "The resolved path of the imported module." /* $COMMENT#JSDOC#RESOLVEDPATH */,
+    "The resolved path of the imported module." /* The resolved path of the imported module. */,
   // library/agnostic20/_commons/utilities/helpers.js
-  ast: "The module's AST (Abstract Syntax Tree)." /* $COMMENT#JSDOC#AST */,
+  ast: "The module's AST (Abstract Syntax Tree)." /* The module's AST (Abstract Syntax Tree). */,
   directiveA20:
-    'The directive as written on top of the file (`"no directive"` if no valid directive).' /* $COMMENT#JSDOC#DIRECTIVEA20 */,
+    'The directive as written on top of the file (`"no directive"` if no valid directive).' /* The directive as written on top of the file (`"no directive"` if no valid directive). */,
   effectiveDirective:
-    "The effective directive of the effective module." /* $COMMENT#JSDOC#EFFECTIVEDIRECTIVE */,
+    "The effective directive of the effective module." /* The effective directive of the effective module. */,
   serverByDefault:
-    "`null` denotes a server-by-default module, ideally a Server Module." /* $COMMENT#JSDOC#SERVERBYDEFAULT */,
+    "`null` denotes a server-by-default module, ideally a Server Module." /* `null` denotes a server-by-default module, ideally a Server Module. */,
   useServer:
-    "`'use server'` denotes a Server Functions Module." /* $COMMENT#JSDOC#USESERVER */,
+    "`'use server'` denotes a Server Functions Module." /* `'use server'` denotes a Server Functions Module. */,
   useClient:
-    "`'use client'` denotes a Client Module." /* $COMMENT#JSDOC#USECLIENT */,
+    "`'use client'` denotes a Client Module." /* `'use client'` denotes a Client Module. */,
   useAgnostic:
-    "`'use agnostic'` denotes an Agnostic Module (formerly Shared Module)." /* $COMMENT#JSDOC#USEAGNOSTIC */,
+    "`'use agnostic'` denotes an Agnostic Module (formerly Shared Module)." /* `'use agnostic'` denotes an Agnostic Module (formerly Shared Module). */,
   useServerLogics20:
-    "`'use server logics'` denotes a Server Logics Module." /* $COMMENT#JSDOC#USESERVERLOGICS20 */,
+    "`'use server logics'` denotes a Server Logics Module." /* `'use server logics'` denotes a Server Logics Module. */,
   useServerComponents20:
-    "`'use server components'` denotes a Server Components Module." /* $COMMENT#JSDOC#USESERVERCOMPONENTS20 */,
+    "`'use server components'` denotes a Server Components Module." /* `'use server components'` denotes a Server Components Module. */,
   useServerFunctions20:
-    "`'use server functions'` denotes a Server Functions Module." /* $COMMENT#JSDOC#USESERVERFUNCTIONS20 */,
+    "`'use server functions'` denotes a Server Functions Module." /* `'use server functions'` denotes a Server Functions Module. */,
   useClientLogics20:
-    "`'use client logics'` denotes a Client Logics Module." /* $COMMENT#JSDOC#USECLIENTLOGICS20 */,
+    "`'use client logics'` denotes a Client Logics Module." /* `'use client logics'` denotes a Client Logics Module. */,
   useClientComponents20:
-    "`'use client components'` denotes a Client Components Module." /* $COMMENT#JSDOC#USECLIENTCOMPONENTS20 */,
+    "`'use client components'` denotes a Client Components Module." /* `'use client components'` denotes a Client Components Module. */,
   useAgnosticLogics20:
-    "`'use agnostic logics'` denotes an Agnostic Logics Module." /* $COMMENT#JSDOC#USEAGNOSTICLOGICS20 */,
+    "`'use agnostic logics'` denotes an Agnostic Logics Module." /* `'use agnostic logics'` denotes an Agnostic Logics Module. */,
   useAgnosticComponents20:
-    "`'use agnostic components'` denotes an Agnostic Components Module." /* $COMMENT#JSDOC#USEAGNOSTICCOMPONENTS20 */,
+    "`'use agnostic components'` denotes an Agnostic Components Module." /* `'use agnostic components'` denotes an Agnostic Components Module. */,
   // library/directive21/_commons/constants/bases.js
   currentFileCommentedDirective:
-    "The current file's commented directive." /* $COMMENT#JSDOC#CURRENTFILECOMMENTEDDIRECTIVE */,
+    "The current file's commented directive." /* The current file's commented directive. */,
   importedFileCommentedDirective:
-    "The imported file's commented directive." /* $COMMENT#JSDOC#IMPORTEDFILECOMMENTEDDIRECTIVE */,
+    "The imported file's commented directive." /* The imported file's commented directive. */,
   // library/directive21/_commons/utilities/helpers.js
-  string: "The original string." /* $COMMENT#JSDOC#STRING */,
-  sourceCode: "The ESLint SourceCode object." /* $COMMENT#JSDOC#SOURCECODE */,
+  string: "The original string." /* The original string. */,
+  sourceCode: "The ESLint SourceCode object." /* The ESLint SourceCode object. */,
   directiveD21:
-    "The commented directive as written on top of the file (cannot be `null` at that stage)." /* $COMMENT#JSDOC#DIRECTIVED21 */,
+    "The commented directive as written on top of the file (cannot be `null` at that stage)." /* The commented directive as written on top of the file (cannot be `null` at that stage). */,
   commentedDirective:
-    "The commented directive of the commented module." /* $COMMENT#JSDOC#COMMENTEDDIRECTIVE */,
+    "The commented directive of the commented module." /* The commented directive of the commented module. */,
   useServerLogics21:
-    "`'use server logics'`, `\"use server logics\"` denoting a Server Logics Module." /* $COMMENT#JSDOC#USESERVERLOGICS21 */,
+    "`'use server logics'`, `\"use server logics\"` denoting a Server Logics Module." /* `'use server logics'`, `"use server logics"` denoting a Server Logics Module. */,
   useClientLogics21:
-    "`'use client logics'`, `\"use client logics\"` denoting a Client Logics Module." /* $COMMENT#JSDOC#USECLIENTLOGICS21 */,
+    "`'use client logics'`, `\"use client logics\"` denoting a Client Logics Module." /* `'use client logics'`, `"use client logics"` denoting a Client Logics Module. */,
   useAgnosticLogics21:
-    "`'use agnostic logics'`, `\"use agnostic logics\"` denoting an Agnostic Logics Module." /* $COMMENT#JSDOC#USEAGNOSTICLOGICS21 */,
+    "`'use agnostic logics'`, `\"use agnostic logics\"` denoting an Agnostic Logics Module." /* `'use agnostic logics'`, `"use agnostic logics"` denoting an Agnostic Logics Module. */,
   useServerComponents21:
-    "`'use server components'`, `\"use server components\"` denoting a Server Components Module." /* $COMMENT#JSDOC#USESERVERCOMPONENTS21 */,
+    "`'use server components'`, `\"use server components\"` denoting a Server Components Module." /* `'use server components'`, `"use server components"` denoting a Server Components Module. */,
   useClientComponents21:
-    "`'use client components'`, `\"use client components\"` denoting a Client Components Module." /* $COMMENT#JSDOC#USECLIENTCOMPONENTS21 */,
+    "`'use client components'`, `\"use client components\"` denoting a Client Components Module." /* `'use client components'`, `"use client components"` denoting a Client Components Module. */,
   useAgnosticComponents21:
-    "`'use agnostic components'`, `\"use agnostic components\"` denoting an Agnostic Components Module." /* $COMMENT#JSDOC#USEAGNOSTICCOMPONENTS21 */,
+    "`'use agnostic components'`, `\"use agnostic components\"` denoting an Agnostic Components Module." /* `'use agnostic components'`, `"use agnostic components"` denoting an Agnostic Components Module. */,
   useServerFunctions21:
-    "`'use server functions'`, `\"use server functions\"` denoting a Server Functions Module." /* $COMMENT#JSDOC#USESERVERFUNCTIONS21 */,
+    "`'use server functions'`, `\"use server functions\"` denoting a Server Functions Module." /* `'use server functions'`, `"use server functions"` denoting a Server Functions Module. */,
   useClientContexts21:
-    "`'use client contexts'`, `\"use client contexts\"` denoting a Client Contexts Module." /* $COMMENT#JSDOC#USECLIENTCONTEXTS21 */,
+    "`'use client contexts'`, `\"use client contexts\"` denoting a Client Contexts Module." /* `'use client contexts'`, `"use client contexts"` denoting a Client Contexts Module. */,
   useAgnosticConditions21:
-    "`'use agnostic conditions'`, `\"use agnostic conditions\"` denoting an Agnostic Conditions Module." /* $COMMENT#JSDOC#USEAGNOSTICCONDITIONS21 */,
+    "`'use agnostic conditions'`, `\"use agnostic conditions\"` denoting an Agnostic Conditions Module." /* `'use agnostic conditions'`, `"use agnostic conditions"` denoting an Agnostic Conditions Module. */,
   useAgnosticStrategies21:
-    "`'use agnostic strategies'`, `\"use agnostic strategies\"` denoting an Agnostic Strategies Module." /* $COMMENT#JSDOC#USEAGNOSTICSTRATEGIES21 */,
+    "`'use agnostic strategies'`, `\"use agnostic strategies\"` denoting an Agnostic Strategies Module." /* `'use agnostic strategies'`, `"use agnostic strategies"` denoting an Agnostic Strategies Module. */,
   useServerLogicsExtensionRule:
-    "`'use server logics'`: Server Logics Modules do NOT export JSX." /* $COMMENT#JSDOC#USESERVERLOGICSEXTENSIONRULE */,
+    "`'use server logics'`: Server Logics Modules do NOT export JSX." /* `'use server logics'`: Server Logics Modules do NOT export JSX. */,
   useClientLogicsExtensionRule:
-    "`'use client logics'`: Client Logics Modules do NOT export JSX." /* $COMMENT#JSDOC#USECLIENTLOGICSEXTENSIONRULE */,
+    "`'use client logics'`: Client Logics Modules do NOT export JSX." /* `'use client logics'`: Client Logics Modules do NOT export JSX. */,
   useAgnosticLogicsExtensionRule:
-    "`'use agnostic logics'`: Agnostic Logics Modules do NOT export JSX." /* $COMMENT#JSDOC#USEAGNOSTICLOGICSEXTENSIONRULE */,
+    "`'use agnostic logics'`: Agnostic Logics Modules do NOT export JSX." /* `'use agnostic logics'`: Agnostic Logics Modules do NOT export JSX. */,
   useServerComponentsExtensionRule:
-    "`'use server components'`: Server Components Modules ONLY export JSX." /* $COMMENT#JSDOC#USESERVERCOMPONENTSEXTENSIONRULE */,
+    "`'use server components'`: Server Components Modules ONLY export JSX." /* `'use server components'`: Server Components Modules ONLY export JSX. */,
   useClientComponentsExtensionRule:
-    "`'use client components'`: Client Components Modules ONLY export JSX." /* $COMMENT#JSDOC#USECLIENTCOMPONENTSEXTENSIONRULE */,
+    "`'use client components'`: Client Components Modules ONLY export JSX." /* `'use client components'`: Client Components Modules ONLY export JSX. */,
   useAgnosticComponentsExtensionRule:
-    "`'use agnostic components'`: Agnostic Components Modules ONLY export JSX." /* $COMMENT#JSDOC#USEAGNOSTICCOMPONENTSEXTENSIONRULE */,
+    "`'use agnostic components'`: Agnostic Components Modules ONLY export JSX." /* `'use agnostic components'`: Agnostic Components Modules ONLY export JSX. */,
   useServerFunctionsExtensionRule:
-    "`'use server functions'`: Server Functions Modules do NOT export JSX." /* $COMMENT#JSDOC#USESERVERFUNCTIONSEXTENSIONRULE */,
+    "`'use server functions'`: Server Functions Modules do NOT export JSX." /* `'use server functions'`: Server Functions Modules do NOT export JSX. */,
   useClientContextsExtensionRule:
-    "`'use client contexts'`: Client Contexts Modules ONLY export JSX." /* $COMMENT#JSDOC#USECLIENTCONTEXTSEXTENSIONRULE */,
+    "`'use client contexts'`: Client Contexts Modules ONLY export JSX." /* `'use client contexts'`: Client Contexts Modules ONLY export JSX. */,
   useAgnosticConditionsExtensionRule:
-    "`'use agnostic conditions'`: Agnostic Conditions Modules ONLY export JSX." /* $COMMENT#JSDOC#USEAGNOSTICCONDITIONSEXTENSIONRULE */,
+    "`'use agnostic conditions'`: Agnostic Conditions Modules ONLY export JSX." /* `'use agnostic conditions'`: Agnostic Conditions Modules ONLY export JSX. */,
   useAgnosticStrategiesExtensionRule:
-    "`'use agnostic strategies'`: Agnostic Strategies Modules may export JSX." /* $COMMENT#JSDOC#USEAGNOSTICSTRATEGIESEXTENSIONRULE */,
+    "`'use agnostic strategies'`: Agnostic Strategies Modules may export JSX." /* `'use agnostic strategies'`: Agnostic Strategies Modules may export JSX. */,
   // tests/_commons/utilities/helpers.js
-  folderPath: "The provided directory." /* $COMMENT#JSDOC#FOLDERPATH */,
+  folderPath: "The provided directory." /* The provided directory. */,
   allFiles:
-    "The accumulator array of file paths. Defaults to an empty array on the initial call, and is passed through each recursive call to be mutated and collect results." /* $COMMENT#JSDOC#ALLFILES */,
+    "The accumulator array of file paths. Defaults to an empty array on the initial call, and is passed through each recursive call to be mutated and collect results." /* The accumulator array of file paths. Defaults to an empty array on the initial call, and is passed through each recursive call to be mutated and collect results. */,
   javaScriptErrorsLength:
-    "The number of errors expected on JavaScript files." /* $COMMENT#JSDOC#JAVASCRIPTERRORSLENGTH */,
+    "The number of errors expected on JavaScript files." /* The number of errors expected on JavaScript files. */,
   typeScriptErrorsLength:
-    "The number of errors expected on TypeScript files." /* $COMMENT#JSDOC#TYPESCRIPTERRORSLENGTH */,
+    "The number of errors expected on TypeScript files." /* The number of errors expected on TypeScript files. */,
   messageId:
-    "The messageId of the errors expected." /* $COMMENT#JSDOC#MESSAGEID */,
+    "The messageId of the errors expected." /* The messageId of the errors expected. */,
 });
 
 // comment variables for tests flavor texts
--- jscomments/_commons/constants/tests.js
+++ jscomments/_commons/constants/tests.js
@@ -9,6 +9,9 @@ export const testsComments = Object.freeze({
     withExtensionParentheses:
       "(with extension)." /* $COMMENT#TESTS#FORCOMPOSEDVARIABLES#WITHEXTENSIONPARENTHESES */,
     byFolder: "by a folder." /* $COMMENT#TESTS#FORCOMPOSEDVARIABLES#BYFOLDER */,
+    viaAlias: "Via alias." /* $COMMENT#TESTS#FORCOMPOSEDVARIABLES#VIAALIAS */,
+    viaBaseUrl:
+      "Via baseUrl." /* $COMMENT#TESTS#FORCOMPOSEDVARIABLES#VIABASEURL */,
     withExtension:
       "with extension" /* $COMMENT#TESTS#FORCOMPOSEDVARIABLES#WITHEXTENSION */,
     importing:
@@ -17,9 +20,6 @@ export const testsComments = Object.freeze({
       "via Special Agnostic Strategy" /* $COMMENT#TESTS#FORCOMPOSEDVARIABLES#VIASTRATEGY */,
     viaStrategyPeriod:
       "via Special Agnostic Strategy." /* $COMMENT#TESTS#FORCOMPOSEDVARIABLES#VIASTRATEGYPERIOD */,
-    viaAlias: "Via alias." /* $COMMENT#TESTS#FORCOMPOSEDVARIABLES#VIAALIAS */,
-    viaBaseUrl:
-      "Via baseUrl." /* $COMMENT#TESTS#FORCOMPOSEDVARIABLES#VIABASEURL */,
     valid: "Valid." /* $COMMENT#TESTS#FORCOMPOSEDVARIABLES#VALID */,
     invalid: "Invalid." /* $COMMENT#TESTS#FORCOMPOSEDVARIABLES#INVALID */,
   }),
