21a584b75a0ab3895f59d6107e75ed92d3491bc8
Luther Tchofo Safo <nintendobserver@gmail.com>
2025-07-23 19:52:06 +0200

    Composed and alias variables made for jsdoc.

--- jscomments/jsdoc/alias-variables.js
+++ jscomments/jsdoc/alias-variables.js
@@ -1,4 +1,6 @@
 export const forAliasVariables = Object.freeze({
+  importsFlow:
+    "The full flow for import traversals to enforce effective directives import rules." /* $COMMENT#JSDOC#FORALIASVARIABLES#IMPORTSFLOW */,
   flowReturnsEarly:
     "Early if the flow needs to be interrupted." /* $COMMENT#JSDOC#FORALIASVARIABLES#FLOWRETURNSEARLY */,
 });
