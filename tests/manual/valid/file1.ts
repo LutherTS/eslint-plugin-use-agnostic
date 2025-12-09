7c3005b833a24a76f09db2637227ea3dd95e5e80
Luther Tchofo Safo <nintendobserver@gmail.com>
2025-12-09 17:47:07 +0100

    Repositioned the valid files...

---
+++ tests/manual/valid/file1.ts
@@ -0,0 +1,12 @@
+// "use server functions"
+
+const x = 1;
+export const y = 2;
+export const z = 3;
+
+export default x;
+
+/* Notes
+Place the file in the directive21/imported/valid folder, use console.debug, and see the directive21 results while running `npm run test`.
+Use a same-level folder called "not-valid" to house the actual files from the "valid" folder in the meantime. 
+*/
